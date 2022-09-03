5747cd06a95af1ddf8e0a67c4ec8ec160866cf10
Minnatulislam <minnatulislam86@gmail.com>
2022-09-03 20:53:52 +0600

    added show all news in landing page of global news

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const modal_body = document.getElementById("modal_body");
 
 // Category option load 
 const loadCategory = async() =>{
-    try {
+  try {
       const url = `https://openapi.programming-hero.com/api/news/categories`;
       const response = await fetch(url);
       const data = await response.json();
@@ -15,6 +15,7 @@ const loadCategory = async() =>{
       throw error;
     }
   }
+  loadCategory();
 
 //   display  Categories function
   const showCategory = categories => {
@@ -89,7 +90,7 @@ const displayNews = alldata => {
 
 
 /* Modal category information */
-function modalCategoryInfo(cId, user_id) {
+const modalCategoryInfo = (cId, user_id) => {
     let url = `https://openapi.programming-hero.com/api/news/category/${cId}`;
     fetch(url)
       .then(res => res.json())
@@ -98,7 +99,7 @@ function modalCategoryInfo(cId, user_id) {
   
 
   /* Modal category information show details */
-  function showData(data, user_id) {
+  const showData = (data, user_id) =>{
     modal_body.innerHTML = "";
     data.data.forEach(data => {
       if (data._id === user_id) {
@@ -117,66 +118,65 @@ function modalCategoryInfo(cId, user_id) {
   }
 
 
-// /* show all news in landing display*/
-// const showAllNews = async () => {
-//     try {
-//       const url = `https://openapi.programming-hero.com/api/news/category/08`;
-//       const res = await fetch(url);
-//       const allData = await res.json();
-//       let dataLength = allData.data;
-//       console.log(dataLength);
-//       if (dataLength.length) {
-//         const p = document.createElement("p");
-//         p.innerHTML = dataLength.length + `   data found`;
-//         countCategories.appendChild(p);
-//       } else {
-//         p.innerHTML = `data not found`;
-//         countCategories.appendChild(p);
-//       }
-//       allData.data.slice(0, 15).forEach(data => {
-//         const div = document.createElement("div");
+/* show all news in landing display*/
+const showAllNews = async () => {
+    try {
+      const url = `https://openapi.programming-hero.com/api/news/category/08`;
+      const res = await fetch(url);
+      const allData = await res.json();
+      let dataLength = allData.data;
+      console.log(dataLength);
+      if (dataLength.length) {
+        const p = document.createElement("p");
+        p.innerHTML = dataLength.length + `   data found`;
+        countCategories.appendChild(p);
+      } else {
+        p.innerHTML = `data not found`;
+        countCategories.appendChild(p);
+      }
+      allData.data.slice(0, 15).forEach(data => {
+        const div = document.createElement("div");
   
-//         div.innerHTML = `
-//           <div class="card mb-3" style="max-width: 540px" >
+        div.innerHTML = `
+          <div class="card mb-3" style="max-width: 540px" >
               
-//           </div>
-//           <div  class="row g-0">
-//           <div class="col-md-4">
-//               <img src=${data.thumbnail_url} class="img-fluid rounded-start" alt="..." />
-//           </div>
-//           <div class="col-md-8">
-//               <div class="card-body">
-//               <h5 class="card-title">${data.title}</h5>
-//               <p class="card-text text-truncate" >
-//                   ${data.details}
-//               </p>
-//               <div class="d-flex align-items-center justify-content-around">
-//               <div>
-//                       <img src=${data.author.img} class="img-fluid rounded-start w-25"  alt="..." />
-//                       <p class="card-text">
-//                           ${data.author.name}
-//                       </p>
-//                       <small> ${data.author.published_date}</small>
-//               </div>
-//               <a onclick= modalCategoryInfo('${data.category_id}', '${data._id}')"  class="btn btn-primary type="button"
-//                   class="btn btn-primary"
-//                   data-bs-toggle="modal"
-//                   data-bs-target="#exampleModal""> details</a>
-//             </div>
-//           </div>
+          </div>
+          <div  class="row g-0">
+          <div class="col-md-4">
+              <img src=${data.thumbnail_url} class="img-fluid rounded-start" alt="..." />
+          </div>
+          <div class="col-md-8">
+              <div class="card-body">
+              <h5 class="card-title">${data.title}</h5>
+              <p class="card-text text-truncate" >
+                  ${data.details}
+              </p>
+              <div class="d-flex align-items-center justify-content-around">
+              <div>
+                      <img src=${data.author.img} class="img-fluid rounded-start w-2"  alt="..." />
+                      <p class="card-text">
+                          ${data.author.name}
+                      </p>
+                      <small> ${data.author.published_date}</small>
+              </div>
+              <a onclick= "modalCategoryInfo('${data.category_id}', '${data._id}', '${data.total_view}')"  class="btn btn-primary type="button"
+                  class="btn btn-primary"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal""> details</a>
+            </div>
+          </div>
               
-//         </div>
-//      </div>
-//         `;
+        </div>
+     </div>
+        `;
   
-//         showCards.appendChild(div);
-//       });
-//     } catch (error) {
-//       console.log(error);
-//       throw error;
-//     }
-//   };
+        showCards.appendChild(div);
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
   
-//   showAllNews();
+  showAllNews();
 
-  loadCategory();
