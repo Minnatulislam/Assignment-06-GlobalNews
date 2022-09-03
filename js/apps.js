54c81b85122c98d29db309b3ef1afcecb36ceca6
Minnatulislam <minnatulislam86@gmail.com>
2022-09-03 18:25:34 +0600

    added display of all news category in global news

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,9 @@
-async function loadCategory() {
+const show_categories = document.getElementById("categories");
+
+
+
+// Category option load 
+const loadCategory = async() =>{
     try {
       const url = `https://openapi.programming-hero.com/api/news/categories`;
       const response = await fetch(url);
@@ -8,4 +13,25 @@ async function loadCategory() {
       throw error;
     }
   }
+
+
+//   display  Categories function
+  const showCategory = categories => {
+    categories.forEach(category => {
+      const CategoryDiv = document.createElement("div");
+      CategoryDiv.innerHTML = `
+      <a style="cursor:pointer;" class="text-primary" onclick=showCategoryNews(${category.category_id})>${category.category_name}</a>
+        `;
+      show_categories.appendChild(CategoryDiv);
+    });
+  }
+  
+//   display news category on UI interface
+  const showCategoryNews = id => {
+    const newsUrl = `https://openapi.programming-hero.com/api/news/category/0${id}`;
+    fetch(newsUrl)
+      .then(res => res.json())
+      .then(data => displayNews(data));
+  }
+
   loadCategory();
